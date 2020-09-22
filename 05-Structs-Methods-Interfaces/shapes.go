27b345914eb20fa659a5719fc127101e4a5a7dbb
Jserrano27 <jserrano.vd@gmail.com>
2020-09-22 19:07:24 -0300

    Methods

--- 05-Structs-Methods-Interfaces/shapes.go
+++ 05-Structs-Methods-Interfaces/shapes.go
@@ -1,14 +1,26 @@
 package structsmethodsinterfaces
 
+import (
+	"math"
+)
+
 type Rectangle struct {
 	Width  float64
 	Height float64
 }
 
-func Perimeter(rectangle Rectangle) float64 {
-	return (rectangle.Height + rectangle.Width) * 2
+func (r Rectangle) Perimeter() float64 {
+	return (r.Height + r.Width) * 2
 }
 
-func Area(rectangle Rectangle) float64 {
-	return rectangle.Height * rectangle.Width
+func (r Rectangle) Area() float64 {
+	return r.Height * r.Width
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * math.Pow(c.Radius, 2)
 }
--- 05-Structs-Methods-Interfaces/shapes_test.go
+++ 05-Structs-Methods-Interfaces/shapes_test.go
@@ -6,7 +6,7 @@ import (
 
 func TestPerimeter(t *testing.T) {
 	rectangle := Rectangle{5.00, 10.00}
-	got := Perimeter(rectangle)
+	got := rectangle.Perimeter()
 	expected := 30.00
 
 	if expected != got {
@@ -15,11 +15,25 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{10.00, 3.00}
-	got := Area(rectangle)
-	expected := 30.00
 
-	if expected != got {
-		t.Errorf("Expected %.2f, but got %.2f instead", expected, got)
-	}
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{10.00, 3.00}
+		got := rectangle.Area()
+		expected := 30.00
+
+		if expected != got {
+			t.Errorf("Expected %.2f, but got %.2f instead", expected, got)
+		}
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{5.00}
+		got := circle.Area()
+		expected := 78.53981633974483
+
+		if expected != got {
+			t.Errorf("Expected %g, but got %g instead", expected, got)
+		}
+	})
+
 }
