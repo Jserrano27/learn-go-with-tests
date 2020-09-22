39f07addffcab7e68617266419b3c69d8e7890ce
Jserrano27 <jserrano.vd@gmail.com>
2020-09-22 19:23:53 -0300

    Interfaces

--- 05-Structs-Methods-Interfaces/shapes_test.go
+++ 05-Structs-Methods-Interfaces/shapes_test.go
@@ -15,25 +15,23 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{10.00, 3.00}
-		got := rectangle.Area()
-		expected := 30.00
-
-		if expected != got {
-			t.Errorf("Expected %.2f, but got %.2f instead", expected, got)
-		}
-	})
-
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{5.00}
-		got := circle.Area()
-		expected := 78.53981633974483
+	checkArea := func(t *testing.T, shape Shape, expected float64) {
+		t.Helper()
+		got := shape.Area()
 
 		if expected != got {
 			t.Errorf("Expected %g, but got %g instead", expected, got)
 		}
+	}
+
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{10.00, 3.00}
+		checkArea(t, rectangle, 30.00)
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{5.00}
+		checkArea(t, circle, 78.53981633974483)
 	})
 
 }
