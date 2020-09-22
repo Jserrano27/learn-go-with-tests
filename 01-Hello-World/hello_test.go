426f00ce36e87579317e375d8347c7e7dd889464
Jserrano27 <jserrano.vd@gmail.com>
2020-09-22 11:32:00 -0300

    Improve tests

--- 01-Hello-World/hello_test.go
+++ 01-Hello-World/hello_test.go
@@ -3,10 +3,23 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := sayHello("Peter")
-	want := "Hello, Peter!"
 
-	if got != want {
-		t.Errorf("got: %q, but wanted: %q", got, want)
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q, but wanted %q", got, want)
+		}
 	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := sayHello("Chris")
+		want := "Hello, Chris!"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("saying 'Hello, World!' when name is not informed", func(t *testing.T) {
+		got := sayHello("")
+		want := "Hello, World!"
+		assertCorrectMessage(t, got, want)
+	})
 }
