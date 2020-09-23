e95f1bd32683203fa366a72bb2b9090ce04b7c02
Jserrano27 <jserrano.vd@gmail.com>
2020-09-22 21:52:45 -0300

    Errors

--- 06-Pointers-and-Errors/wallet_test.go
+++ 06-Pointers-and-Errors/wallet_test.go
@@ -1,16 +1,10 @@
 package pointers
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestWallet(t *testing.T) {
-
-	assertBalance := func(t *testing.T, wallet Wallet, expected Bitcoin) {
-		t.Helper()
-		got := wallet.Balance()
-		if expected != got {
-			t.Errorf("Expected %s, but got %s instead", got, expected)
-		}
-	}
 
 	t.Run("Deposit", func(t *testing.T) {
 		wallet := Wallet{}
@@ -18,10 +12,47 @@ func TestWallet(t *testing.T) {
 		assertBalance(t, wallet, Bitcoin(20))
 	})
 
-	t.Run("Withdraw", func(t *testing.T) {
+	t.Run("Withdraw with funds", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
-		wallet.Withdraw(Bitcoin(15))
+		err := wallet.Withdraw(Bitcoin(15))
+
 		assertBalance(t, wallet, Bitcoin(5))
+		assertNoError(t, err)
 	})
 
+	t.Run("Withdraw when insufficient funds", func(t *testing.T) {
+		startingBalance := Bitcoin(20)
+		wallet := Wallet{balance: startingBalance}
+		err := wallet.Withdraw(Bitcoin(25))
+
+		assertBalance(t, wallet, startingBalance)
+		assertError(t, err, ErrInsufficientFunds)
+	})
+}
+
+func assertBalance(t *testing.T, wallet Wallet, expected Bitcoin) {
+	t.Helper()
+	got := wallet.Balance()
+	if expected != got {
+		t.Errorf("Expected %s, but got %s instead", expected, got)
+	}
+}
+
+func assertError(t *testing.T, err error, expected error) {
+	t.Helper()
+
+	if err == nil {
+		t.Fatal("WARNING: Expected an error but didn't get one")
+	}
+
+	if expected != err {
+		t.Errorf("Expected error %q, but got %q instead", expected, err)
+	}
+}
+
+func assertNoError(t *testing.T, err error) {
+	t.Helper()
+	if err != nil {
+		t.Fatal("WARNING: Error was not expected but it got one")
+	}
 }
