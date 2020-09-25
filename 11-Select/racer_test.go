5b7f8697059d2bc4bbe25565b53008173339e8b9
Jserrano27 <jserrano.vd@gmail.com>
2020-09-25 15:39:43 -0300

    include goroutine, channels, select and delay

--- 11-Select/racer_test.go
+++ 11-Select/racer_test.go
@@ -8,21 +8,42 @@ import (
 )
 
 func TestRacer(t *testing.T) {
-	slowServer := createDelayedServer(100 * time.Millisecond)
-	fastServer := createDelayedServer(0 * time.Millisecond)
 
-	defer slowServer.Close()
-	defer fastServer.Close()
+	t.Run("returns the fastest url", func(t *testing.T) {
+		slowServer := createDelayedServer(100 * time.Millisecond)
+		fastServer := createDelayedServer(0 * time.Millisecond)
 
-	slowURL := slowServer.URL
-	fastURL := fastServer.URL
+		defer slowServer.Close()
+		defer fastServer.Close()
 
-	want := fastURL
-	got := Racer(slowURL, fastURL)
+		slowURL := slowServer.URL
+		fastURL := fastServer.URL
 
-	if got != want {
-		t.Errorf("Got %q, but expected was %q", got, want)
-	}
+		want := fastURL
+		got, err := Racer(slowURL, fastURL)
+
+		if err != nil {
+			t.Fatalf("Did not expect an error but got one %v", err)
+		}
+
+		if got != want {
+			t.Errorf("Got %q, but expected was %q", got, want)
+		}
+	})
+
+	t.Run("returns an error if servers doesn't respond within the specified timeout", func(t *testing.T) {
+		server := createDelayedServer(5 * time.Millisecond)
+
+		defer server.Close()
+
+		url := server.URL
+
+		_, err := ConfigurableRacer(url, url, 1*time.Millisecond)
+
+		if err == nil {
+			t.Error("Expecting an error but got none")
+		}
+	})
 }
 
 func createDelayedServer(delay time.Duration) *httptest.Server {
