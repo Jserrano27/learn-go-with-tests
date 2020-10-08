06c6a8e75d25b9951f2a0883011f2cb8d3b68faa
Jserrano27 <jserrano.vd@gmail.com>
2020-10-08 16:38:00 -0300

    bugfix

--- App/main.go
+++ App/main.go
@@ -12,7 +12,7 @@ func main() {
 	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0644)
 
 	if err != nil {
-		log.Fatalf("problema opening %s: %v", dbFileName, err)
+		log.Fatalf("problem opening %s: %v", dbFileName, err)
 	}
 
 	store, err := NewFileSystemPlayerStore(db)
